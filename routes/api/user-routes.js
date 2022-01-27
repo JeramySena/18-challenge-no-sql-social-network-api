0442d2829dab2cd7ceac7cc5ec35942514d4c23a
Jeramy Sena <jeramy_sena@hotmail.com>
2022-01-27 15:17:33 -0600

    added back routes and server code

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,29 @@
+const router = require('express').Router();
+const { route } = require('express/lib/application');
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    addFriend,
+    updateUser,
+    deleteUser,
+    removeFriend
+  } = require('../../controllers/user-controller');
 
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+route
+  .route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend)
+
+module.exports = router;
