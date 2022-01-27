377137a257823638a477fe8e8a6ded2ec0a768d8
Jeramy Sena <jeramy_sena@hotmail.com>
2022-01-27 14:10:02 -0600

    created user model

---
+++ models/Users.js
@@ -0,0 +1,47 @@
+const { Schema, User } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+UserSchema.virtual('friendsCount').get(function () {
+    return this.friends.lenght;
+});
+
+const User = model('User', UserSchema);
+
+module.export = User;
