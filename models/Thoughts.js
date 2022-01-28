b238e75996869e838e62411b087c482ead65b6e8
Jeramy Sena <jeramy_sena@hotmail.com>
2022-01-28 13:32:08 -0600

    added thoughts model

---
+++ models/Thoughts.js
@@ -0,0 +1,63 @@
+const { Schema, model, Types } = require("mongoose");
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const ThoughtsSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+ThoughtsSchema.virtual("reactionsCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thoughts = model("Thought", ThoughtsSchema);
+
+module.exports = Thoughts;
